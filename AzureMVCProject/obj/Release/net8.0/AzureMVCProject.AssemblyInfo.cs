a0aa961b46ebe9c75aa8a4ec89fdafa980ea25f0
yaseentest1 <yaseentest1@outlook.com>
2024-02-24 23:18:27 +0530

    test

--- AzureMVCProject/obj/Release/net8.0/AzureMVCProject.AssemblyInfo.cs
+++ AzureMVCProject/obj/Release/net8.0/AzureMVCProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureMVCProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aee0279f0273eb21135e332cfbcdadea009ae7b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureMVCProject")]
